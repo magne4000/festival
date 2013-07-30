65bbe6393234fc1de6358c4f428ad3d0e64b22b3
Joel Charles <joel.charles@gmail.com>
2013-07-30 22:20:51 +0200

    Mongodb test

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express =
+	require('express'),
+	routes = require('./routes'),
+	user = require('./routes/user'),
+	http = require('http'),
+	path = require('path'),
+	mongoose = require('mongoose');
 
 var app = express();
 
@@ -24,12 +25,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+
+	mongoose.connect('mongodb://localhost/test');
+
+	var kittySchema = mongoose.Schema({
+		name : String
+	});
+	mongoose.model('Kitten', kittySchema);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+	res.render('index', {
+		title : 'Express'
+	});
+
+	var mongoose = require('mongoose');
+	var Kitten = mongoose.model('Kitten');
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	var silence = new Kitten({
+		name : 'Silence'
+	});
+	silence.save();
+	Kitten.find(function(err, kittens) {
+		console.log(kittens);
+	});
 };
