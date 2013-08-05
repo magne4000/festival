6628ad7362f3555290fb6220f9576821bc4f4c8e
Joel Charles <joel.charles@gmail.com>
2013-08-05 16:46:48 +0200

    Tracks model

---
+++ models/track.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var trackSchema = new Schema({
+	path: {type: String, 'default': '', trim: true, required: true, unique: true},
+	genre: {type: String, 'default': '', trim: true},
+	album: {type: String, 'default': '', trim: true},
+	name: {type: String, 'default': '', trim: true, required: true},
+	duration: {type: Number},
+	year: {type: Number, min: 0, max: 9999},
+	bitrate: {type: Number},
+	frequency: {type: Number},
+	trackno: {type: Number},
+	last_updated: {type: Date, 'default': Date.now}
+});
+
+trackSchema.pre('save', function(next) {
+	this.last_updated = Date.now();
+	next();
+});
+
+mongoose.model('track', trackSchema);
