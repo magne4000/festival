660a94d889a045e4c29526c2356ddd3f69e27dd7
Joel Charles <joel.charles@gmail.com>
2013-08-06 13:21:57 +0200

    Index route

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,14 @@
 /*
  * GET home page.
  */
+var mongoose = require('mongoose'),
+	db = mongoose.connection,
+	settings = require('../settings.json');
 
 exports.index = function(req, res) {
 	res.render('index', {
-		title : 'Express'
+		title : 'Webmusic',
+		soundmanager: settings.soundmanager,
+		debug: settings.debug
 	});
-	/*
-	var mongoose = require('mongoose');
-	var Kitten = mongoose.model('Kitten');
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	var silence = new Kitten({
-		name : 'Silence'
-	});
-	silence.save();
-	Kitten.find(function(err, kittens) {
-		console.log(kittens);
-	});*/
-	
-	/*
-	var taglib = require('taglib');
-	taglib.read('/home/magne/music/01 The Business Of Living.mp3', function(err, tag, audioProperties){
-		console.log(tag);
-		console.log(audioProperties);
-	});*/
 };
