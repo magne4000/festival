f11d57213831b22078153014742ec1f40bc6bf65
Joel Charles <joel.charles@gmail.com>
2013-08-06 13:18:59 +0200

    .

--- app.js
+++ app.js
@@ -5,15 +5,19 @@
 var express =
 	require('express'),
 	routes = require('./routes'),
-	user = require('./routes/user'),
+	ajax = require('./routes/ajax'),
+	music = require('./routes/music'),
+	scanner = require('./scanner'),
 	http = require('http'),
 	path = require('path'),
-	mongoose = require('mongoose');
+	config = require('./settings.json'),
+	mongoose = require('mongoose'),
+	fs = require('fs');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || config.express.port || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -29,15 +33,22 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/ajax/menu', ajax.menu);
+app.get('/music/:id', music.index);
+
+mongoose.connect(config.mongodb.url);
+
+//Bootstrap models
+var models_path = __dirname + '/models';
+fs.readdirSync(models_path).forEach(function(file) {
+	if (~file.indexOf('.js')) {
+		require(models_path + '/' + file);
+	}
+});
+
+//Watcher
+scanner.watch();
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
-
-	mongoose.connect('mongodb://localhost/test');
-
-	var kittySchema = mongoose.Schema({
-		name : String
-	});
-	mongoose.model('Kitten', kittySchema);
 });
