9dd0d76d26681322fb8ad27ea4f4c7adab26890f
Joël Charles <joel.charles91@gmail.com>
2014-04-01 18:09:09 +0200

    overlay class created. It will be used to display loading overlay on ajax call

--- public/js/views.js
+++ public/js/views.js
@@ -4,6 +4,17 @@ var postTimeout = {
     albums: null
 };
 
+function hideLoadingOverlay(applyOn) {
+    $(applyOn).find('.overlay').remove();
+}
+
+function showLoadingOverlay(applyOn) {
+    var overlayHtml = "<div class='overlay'><div class='spinner'>"+
+        "<div class='rect1'></div><div class='rect2'></div><div class='rect3'>"+
+        "</div><div class='rect4'></div><div class='rect5'></div></div></div>";
+    $(overlayHtml).appendTo($(applyOn));
+}
+
 function hideVolume(){
     $('#player .controls .volume').removeClass("active");
     $('.volume-wrapper').addClass('hidden');
