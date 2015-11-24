3b06a81e574a3825501d0f8b4ea5004a75b4ec82
Jol Charles <joel.charles91@gmail.com>
2015-11-24 17:00:46 +0100

    Delete print

--- scanner.py
+++ scanner.py
@@ -32,7 +32,6 @@ class CoverThread(Thread):
 
     def rescan_albums_without_cover(self):
         last_cover_scan = info.Infos.get('last_cover_scan')
-        print(last_cover_scan + app.config['COVERS_FETCH_ONLINE_INTERVAL'], datetime.now())
         if last_cover_scan is not None:
             return last_cover_scan + app.config['COVERS_FETCH_ONLINE_INTERVAL'] <= datetime.now()
         return False
